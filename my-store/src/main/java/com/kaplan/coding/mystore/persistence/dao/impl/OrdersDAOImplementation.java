0cfece50cdbb8973cac93447e8c7a245990b8dfb
njahan <nusrat.jahan@kaplan.com>
2016-09-22 13:28:07 -0400

    Orders DAO

---
+++ my-store/src/main/java/com/kaplan/coding/mystore/persistence/dao/impl/OrdersDAOImplementation.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.kaplan.coding.mystore.persistence.dao.impl;
+
+import java.util.List;
+
+import com.kaplan.coding.mystore.persistence.dao.OrdersDAO;
+import com.kaplan.coding.mystore.persistence.domain.Orders;
+
+/**
+ * @author NJahan
+ *
+ */
+public class OrdersDAOImplementation implements OrdersDAO {
+
+	/* (non-Javadoc)
+	 * @see com.kaplan.coding.mystore.persistence.dao.OrdersDAO#save(com.kaplan.coding.mystore.persistence.domain.Orders)
+	 */
+	@Override
+	public Boolean save(Orders orders) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.kaplan.coding.mystore.persistence.dao.OrdersDAO#update(com.kaplan.coding.mystore.persistence.domain.Orders)
+	 */
+	@Override
+	public Boolean update(Orders orders) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.kaplan.coding.mystore.persistence.dao.OrdersDAO#findById(java.lang.Integer)
+	 */
+	@Override
+	public Orders findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.kaplan.coding.mystore.persistence.dao.OrdersDAO#getAll()
+	 */
+	@Override
+	public List<Orders> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
