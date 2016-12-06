730bc801049e485386f76929e6396e28826c5a22
imran-ahmed-kaplan <imran.ahmed@kaplan.com>
2016-12-06 14:41:15 -0500

    test products
    test products

--- my-store/src/main/java/com/kaplan/coding/mystore/business/test/TestProducts.java
+++ my-store/src/main/java/com/kaplan/coding/mystore/business/test/TestProducts.java
@@ -24,7 +24,7 @@ public class TestProducts {
     public void testingProduct(){
         ProductService testProductService = null;
         log.debug("test products");
-        testProductService.getAllProducts();
+        testProductService.getAllProducts(); 
         
     }
 
