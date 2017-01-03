b733f2a85821a9ed383bbf3f2bcf07f60744289c
njahan <nusrat.jahan@kaplan.com>
2017-01-03 13:07:47 -0500

    Nusrat Local changes.
    Nusrat Local changes.

--- my-store/src/main/java/com/kaplan/coding/mystore/business/test/TestProducts.java
+++ my-store/src/main/java/com/kaplan/coding/mystore/business/test/TestProducts.java
@@ -4,27 +4,33 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.kaplan.coding.mystore.business.impl.ProductService;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
         "file:src/main/webapp/WEB-INF/spring/daoApplicationContext.xml",
         "file:src/main/webapp/WEB-INF/spring/serviceApplicationContext.xml" })
 public class TestProducts {
+    @Autowired
+    private ProductService productService ;
     private static final Logger log = Logger.getLogger(TestProducts.class);
-    static{DOMConfigurator.configure("src/main/webapp/WEB-INF/log4j/log4j-config-minimal.xml");}
+    static{DOMConfigurator.configure("file:src/main/webapp/WEB-INF/log4j/log4j-config-minimal.xml");}
     
 
     
     @SuppressWarnings("null")
     @Test
+    
     public void testingProduct(){
-        ProductService testProductService = null;
+        
         log.debug("test products");
-        testProductService.getAllProducts();
+        productService.getAllProducts();
         
     }
 
--- my-store/src/main/java/com/kaplan/coding/mystore/business/util/Transformer.java
+++ my-store/src/main/java/com/kaplan/coding/mystore/business/util/Transformer.java
@@ -4,6 +4,8 @@
 package com.kaplan.coding.mystore.business.util;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.junit.Test;
 
 import com.kaplan.coding.mystore.business.test.TestProducts;
 import com.kaplan.coding.mystore.business.vo.ProductVo;
@@ -15,12 +17,16 @@ import com.kaplan.coding.mystore.persistence.domain.Product;
  *
  */
 public final class Transformer {
-    private static final Logger log = Logger.getLogger(ProductVo.class);
+    private static final Logger log = Logger.getLogger(Transformer.class);
+    static{DOMConfigurator.configure("src/main/webapp/WEB-INF/log4j/log4j-config-minimal.xml");}
+    
+    
 
     public static ProductVo convert(Product product ){
         return null;
         //TODO: add code here!!!
-        //log.debug("test productsVo");
+        
+       //log.debug("TEST");
       
         
     }
