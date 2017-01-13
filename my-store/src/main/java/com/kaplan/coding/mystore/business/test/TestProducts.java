5f24bbda12daf49776939d2c1b8257d08ab95602
Juan Cruz <Juan.Cruz@kaplan.com>
2017-01-13 11:04:17 -0500

    Fixing errors
    Fixing errors

--- my-store/src/main/java/com/kaplan/coding/mystore/business/test/TestProducts.java
+++ my-store/src/main/java/com/kaplan/coding/mystore/business/test/TestProducts.java
@@ -10,28 +10,24 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.kaplan.coding.mystore.business.impl.ProductService;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-        "file:src/main/webapp/WEB-INF/spring/daoApplicationContext.xml",
-        "file:src/main/webapp/WEB-INF/spring/serviceApplicationContext.xml" })
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/daoApplicationContext.xml",
+		"file:src/main/webapp/WEB-INF/spring/serviceApplicationContext.xml" })
 public class TestProducts {
-    @Autowired
-    private ProductService productService ;
-    private static final Logger log = Logger.getLogger(TestProducts.class);
-    
-    static{DOMConfigurator.configure("src/main/webapp/WEB-INF/log4j/log4j-config-minimal.xml");}
-   
-    @SuppressWarnings("null")
-    @Test
-    
-    public void testingProduct(){
-        
-        ProductService testProductService = null;
-        log.debug("test products");
-        testProductService.getAllProducts(); 
-        
-       
-    }
+	@Autowired
+	private ProductService productService;
+	private static final Logger log = Logger.getLogger(TestProducts.class);
+
+	static {
+		log.debug("loading Log4J Config");
+		DOMConfigurator.configure("src/main/webapp/WEB-INF/log4j/log4j-config-minimal.xml");
+		log.debug("Log4J Config loaded");
+	}
+
+	@Test
+	public void testingProduct() {
+		log.debug("test retrieving products - Invoke ProductService");
+		productService.getAllProducts();
+	}
 
 }
